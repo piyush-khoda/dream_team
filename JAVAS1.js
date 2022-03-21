688c187c27f547c0105d41cdbbe58d788f6fa904
piyush <er.piyush.khoda@gmail.com>
2022-03-20 19:33:07 -0700

    first commit

---
+++ JAVAS1.js
@@ -0,0 +1,10 @@
+var number1 = 34;
+var number2 = 56;
+console.log(number1 * number2);
+console.log(number1);
+console.log(number1 + number2);
+var arr = [1,2,3,4,5]
+console.log(arr)
+console.log ("the value of number2 - number1", number2 - number1);
+console.log(number1 == number2);
+console.log(number1 <= number2);
